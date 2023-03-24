d463040a22e2ed84291804e417919358a597b922
jaksonqwik <ndryg547@gmail.com>
2023-03-24 16:48:10 +0200

    strat

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+require_once 'config.php';
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Task Manager</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    
+</body>
+</html>
