cb832ab98222a796641a22d867e12b28e9717b83
jaksonqwik <ndryg547@gmail.com>
2023-03-24 16:48:57 +0200

    bd

--- config.php
+++ config.php
@@ -3,5 +3,3 @@ $host = 'localhost'; // адреса сервера бази даних
 $dbname = 'user'; // назва бази даних
 $user = 'root'; // користувач бази даних
 $password = ''; // пароль користувача бази даних
-
-$connect = mysql_connect()
