734ea1a1e5a2e3ebbcf5b649e44307d659f55c4e
jaksonqwik <ndryg547@gmail.com>
2023-03-25 16:22:34 +0200

    status

--- title.php
+++ title.php
@@ -1,18 +1,17 @@
 <?php
-$host = 'localhost'; // adress server DB
-$dbname = 'user'; // name DB
-$user = 'root'; // user name
-$password = ''; // pass
+$host = 'localhost'; 
+$dbname = 'user'; 
+$user = 'root'; 
+$password = '';
 
 $connect = new mysqli($host, $user, $password, $dbname);
-$sql = "SELECT * FROM `users`";
+$sql = "SELECT * FROM `task`";
 $res = $connect->query($sql);
 $user = array();
 while ($row = $res->fetch_assoc()) {
     $user[] = $row;
 }
 $connect->close();
-
 ?>
 
 <!DOCTYPE html>
@@ -27,5 +26,34 @@ $connect->close();
 <body>
     <a href="/creat_task.php">Создать проект</a>
     <a href="/index.php">Выход</a>
+    <form action="" method="POST">
+        <div>
+            <?php
+                foreach ($user as $row) {
+                    if (!isset($_POST['button']) && !isset($_POST['succesfull'])) {
+                        echo "<p name='status'>" . $row['status'] . "</p>";
+                    } else {
+                        $new_status = $row['status']; 
+                        if (isset($_POST['button']) && isset($_POST['task_id']) && $_POST['task_id'] == $row['id']) {
+                            $new_status = "В роботе";
+                        } elseif (isset($_POST['succesfull']) && isset($_POST['task_id']) && $_POST['task_id'] == $row['id']) {
+                            $new_status = "Выполнено";
+                        }
+                        echo "<p>" . $new_status . "</p>";
+                    }
+                    echo "<p>" . $row['titel'] . "</p>";
+                    echo "<p>" . $row['text_task'] . "</p>";
+                    if (!empty($row['file'])) {
+                        echo '<a href="' . $row['file'] . '">Скачать файл</a>';
+                    }
+                    echo '<form action="" method="POST">';
+                    echo '<input type="hidden" name="task_id" value="' . $row['id'] . '">';
+                    echo '<input type="submit" name="button" value="Принять">';
+                    echo '<input type="submit" name="succesfull" value="Выполнил">';
+                    echo '</form>';
+                }
+            ?>
+        </div>
+    </form>
 </body>
 </html>
